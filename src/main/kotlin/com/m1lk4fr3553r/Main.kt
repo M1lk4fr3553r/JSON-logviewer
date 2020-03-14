17a7159383aa033f3cd011f4776245ad3bd2298f
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-03-14 22:01:40 +0100

    Export properties from jar

--- src/main/kotlin/com/m1lk4fr3553r/Main.kt
+++ src/main/kotlin/com/m1lk4fr3553r/Main.kt
@@ -1,8 +1,15 @@
 package com.m1lk4fr3553r
 
 import com.m1lk4fr3553r.controller.MainWindowController
+import java.io.File
+import java.io.FileOutputStream
 
 fun main(args: Array<String>) {
+    val stream = Util::class.java.classLoader.getResourceAsStream("_JSONlogviewer.properties")
+    val outfile = File(File(Util::class.java.getProtectionDomain().getCodeSource().getLocation().toURI()).parent + File.separator + "JSONlogviewer.properties")
+    if (!outfile.exists() && stream != null) {
+        FileOutputStream(outfile).write(stream.readAllBytes())
+    }
     val controller = MainWindowController()
 
     if (args.isNotEmpty()) {
--- src/main/kotlin/com/m1lk4fr3553r/Util.kt
+++ src/main/kotlin/com/m1lk4fr3553r/Util.kt
@@ -1,11 +1,14 @@
 package com.m1lk4fr3553r
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.util.*
 
 class Util {
     companion object {
         fun getProperties(): Properties {
-            val stream = Util::class.java.classLoader.getResourceAsStream("JSONlogviewer.properties")
+            val stream = FileInputStream(File(File(Util::class.java.getProtectionDomain().getCodeSource().getLocation().toURI()).parent + File.separator + "JSONlogviewer.properties"))
             val prop = Properties()
             prop.load(stream)
             return prop
