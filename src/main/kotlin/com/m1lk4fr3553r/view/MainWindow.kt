9e64af484ef6caee923bf2da9d0932d33d305668
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-03-14 16:19:34 +0100

    Add filter input

--- src/main/kotlin/com/m1lk4fr3553r/view/MainWindow.kt
+++ src/main/kotlin/com/m1lk4fr3553r/view/MainWindow.kt
@@ -1,25 +1,28 @@
 package com.m1lk4fr3553r.view
 
 import com.m1lk4fr3553r.model.JSONListItem
+import java.awt.BorderLayout
 import javax.swing.*
 
 class MainWindow(title: String): JFrame() {
     val list = JList<JSONListItem>()
     val scrollPane: JScrollPane
+    val statusBar = JToolBar()
+    val filterField = JTextField()
     init {
+        initWindow(title)
+        list.setCellRenderer(ItemListCellRenderer())
+        scrollPane = JScrollPane(list)
+        add(scrollPane)
+        statusBar.add(filterField)
+        add(statusBar, BorderLayout.SOUTH)
+    }
+
+    fun initWindow(title: String) {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
         setTitle(title)
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(1920/2, 1080/2)
         setLocationRelativeTo(null)
-
-        val listModel = DefaultListModel<JSONListItem>()
-        for (i in 0..20) {
-            listModel.addElement(JSONListItem())
-        }
-        list.model = listModel
-        list.setCellRenderer(ItemListCellRenderer())
-        scrollPane = JScrollPane(list)
-        add(scrollPane)
     }
 }
