23ec5311f8b65b09e8eb771996edc76ea998922c
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-03-14 21:41:07 +0100

    Unescape text in detail view

--- src/main/kotlin/com/m1lk4fr3553r/view/ItemView.kt
+++ src/main/kotlin/com/m1lk4fr3553r/view/ItemView.kt
@@ -2,6 +2,7 @@ package com.m1lk4fr3553r.view
 
 import com.m1lk4fr3553r.controller.ItemViewController
 import com.m1lk4fr3553r.model.JSONListItem
+import org.apache.commons.text.StringEscapeUtils
 import javax.swing.JFrame
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
@@ -18,7 +19,7 @@ class ItemView(item: JSONListItem, parent: JFrame) : JFrame() {
         setLocationRelativeTo(parent)
         isVisible = true
 
-        textArea = JTextArea(item.json.toString(2))
+        textArea = JTextArea(StringEscapeUtils.unescapeJava(item.json.toString(2) ))
         textArea.isEditable = false
         add(JScrollPane(textArea))
     }
