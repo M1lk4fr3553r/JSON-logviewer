6af1d972c4d30f445a4c6d7b462515073a6f6e29
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-05-13 18:09:24 +0200

    Move color load into getCellRenderer to support live reload

--- src/main/kotlin/com/m1lk4fr3553r/view/ItemListCellRenderer.kt
+++ src/main/kotlin/com/m1lk4fr3553r/view/ItemListCellRenderer.kt
@@ -4,22 +4,17 @@ import com.m1lk4fr3553r.util.Util
 import com.m1lk4fr3553r.model.JSONListItem
 import java.awt.Color
 import java.awt.Component
+import java.time.LocalDateTime
 import javax.swing.DefaultListCellRenderer
 import javax.swing.JList
 
 class ItemListCellRenderer : DefaultListCellRenderer() {
-    val infoColor: Color
-    val warnColor: Color
-    val errorColor: Color
-    val debugColor: Color
-    init {
-        val props = Util.getProperties()
-        infoColor = Color.decode(props.getProperty("color.info", "#000000"))
-        warnColor = Color.decode(props.getProperty("color.warn", "#0000FF"))
-        errorColor = Color.decode(props.getProperty("color.error", "#FF0000"))
-        debugColor = Color.decode(props.getProperty("color.debug", "#00FF00"))
-    }
     override fun getListCellRendererComponent(list: JList<*>?, value: Any?, index: Int, isSelected: Boolean, cellHasFocus: Boolean): Component {
+        val props = Util.getProperties()
+        val infoColor = Color.decode(props.getProperty("color.info", "#000000"))
+        val warnColor = Color.decode(props.getProperty("color.warn", "#ada401"))
+        val errorColor = Color.decode(props.getProperty("color.error", "#aa0000"))
+        val debugColor = Color.decode(props.getProperty("color.debug", "#0037dd"))
         val item = (value as JSONListItem)
         val component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
         when (item.items.get(item.levelKey)) {
