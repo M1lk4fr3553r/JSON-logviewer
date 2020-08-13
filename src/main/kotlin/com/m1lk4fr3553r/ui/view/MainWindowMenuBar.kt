a5b38cb4adac6a40bbfdd9703b16723be5002354
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-08-13 23:28:00 +0200

    Add about menu
    resolves #36

--- src/main/kotlin/com/m1lk4fr3553r/ui/view/MainWindowMenuBar.kt
+++ src/main/kotlin/com/m1lk4fr3553r/ui/view/MainWindowMenuBar.kt
@@ -1,46 +1,64 @@
 package com.m1lk4fr3553r.ui.view
 
 import com.m1lk4fr3553r.ui.controller.MainWindowController
+import com.m1lk4fr3553r.util.Globals
 import javax.swing.JMenu
 import javax.swing.JMenuBar
 import javax.swing.JMenuItem
+import javax.swing.JOptionPane
 import kotlin.system.exitProcess
 
-class MainWindowMenuBar(private val controller: MainWindowController): JMenuBar() {
-    init {
-        initFileMenu()
-        initEditMenu()
+class MainWindowMenuBar(private val controller: MainWindowController) : JMenuBar() {
+  init {
+    initFileMenu()
+    initEditMenu()
+    initAboutMenu()
+  }
+
+  private fun initFileMenu() {
+    val fileMenu = JMenu("File")
+
+    val openItem = JMenuItem("Open")
+    openItem.addActionListener { controller.loadFileWithOpenDialog() }
+    fileMenu.add(openItem)
+
+    val settingsIcon = JMenuItem("Settings")
+    settingsIcon.addActionListener { controller.openSettings() }
+    fileMenu.add(settingsIcon)
+
+    val exitItem = JMenuItem("Exit")
+    exitItem.addActionListener { exitProcess(0) }
+    fileMenu.add(exitItem)
+
+    add(fileMenu)
+  }
+
+  private fun initEditMenu() {
+    val editMenu = JMenu("Edit")
+
+    val filterItem = JMenuItem("Filter")
+    filterItem.addActionListener { controller.requestFilter() }
+    editMenu.add(filterItem)
+
+    val searchItem = JMenuItem("Search")
+    searchItem.addActionListener { controller.requestSearch() }
+    editMenu.add(searchItem)
+
+    add(editMenu)
+  }
+
+  private fun initAboutMenu() {
+    val aboutMenu = JMenu("About")
+
+    val versionItem = JMenuItem("Version")
+    versionItem.addActionListener {
+      JOptionPane.showMessageDialog(this,
+          "Version: ${Globals.VERSION}\n" +
+              "Build: ${Globals.BUILD}\n" +
+              "Build date: ${Globals.BUILD_DATE}")
     }
+    aboutMenu.add(versionItem)
 
-    private fun initFileMenu() {
-        val fileMenu = JMenu("File")
-
-        val openItem = JMenuItem("Open")
-        openItem.addActionListener { controller.loadFileWithOpenDialog() }
-        fileMenu.add(openItem)
-
-        val settingsIcon = JMenuItem("Settings")
-        settingsIcon.addActionListener { controller.openSettings() }
-        fileMenu.add(settingsIcon)
-
-        val exitItem = JMenuItem("Exit")
-        exitItem.addActionListener { exitProcess(0) }
-        fileMenu.add(exitItem)
-
-        add(fileMenu)
-    }
-
-    private fun initEditMenu() {
-        val editMenu = JMenu("Edit")
-
-        val filterItem = JMenuItem("Filter")
-        filterItem.addActionListener { controller.requestFilter() }
-        editMenu.add(filterItem)
-
-        val searchItem = JMenuItem("Search")
-        searchItem.addActionListener { controller.requestSearch() }
-        editMenu.add(searchItem)
-
-        add(editMenu)
-    }
+    add(aboutMenu)
+  }
 }
