6025a85525257a71bdfcad58eba4cb2b2d95c2bc
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-08-14 00:10:00 +0200

    Make it possible to color the background
    resolves #37

--- src/main/kotlin/com/m1lk4fr3553r/model/properties/Colors.kt
+++ src/main/kotlin/com/m1lk4fr3553r/model/properties/Colors.kt
@@ -6,19 +6,27 @@ import kotlinx.serialization.Serializable
 @Serializable
 class Colors : DisplayableSettings {
     var debug: String = "#0037dd"
+    var debugBackground: String = "#FFFFFF"
     var error: String = "#aa0000"
+    var errorBackground: String = "#FFFFFF"
     var info: String = "#000000"
+    var infoBackground: String = "#FFFFFF"
     var search: String = "#9c9e67"
     var warn: String = "#ada401"
+    var warnBackground: String = "#FFFFFF"
     var colorEntireRow: Boolean = false
 
     override fun getKeys(): Array<String> {
         return arrayOf(
                 "debug",
+                "debugBackground",
                 "error",
+                "errorBackground",
                 "info",
+                "infoBackground",
                 "search",
                 "warn",
+                "warnBackground",
                 "colorEntireRow"
         )
     }
@@ -26,20 +34,28 @@ class Colors : DisplayableSettings {
     override fun getValues(): Array<String> {
         return arrayOf(
                 debug,
+                debugBackground,
                 error,
+                errorBackground,
                 info,
+                infoBackground,
                 search,
                 warn,
+                warnBackground,
                 colorEntireRow.toString()
         )
     }
 
     override fun setValues(values: Array<String>) {
         debug = values[0]
-        error = values[1]
-        info = values[2]
-        search = values[3]
-        warn = values[4]
-        colorEntireRow = values[5].toBoolean()
+        debugBackground = values[1]
+        error = values[2]
+        errorBackground = values[3]
+        info = values[4]
+        infoBackground = values[5]
+        search = values[6]
+        warn = values[7]
+        warnBackground = values[8]
+        colorEntireRow = values[9].toBoolean()
     }
 }
